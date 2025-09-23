e6d4a38fbcab1f89330c5a6e5806ab9dfac51901
Murilo Cavalcanti <murilo@omotor.com.br>
2025-09-22 22:14:24 -0300

    angular project first structure
    - DEV branch created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LobbyComponent} from "./routes/lobby/lobby.component";
+import {LoginComponent} from "./routes/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LobbyComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: 'login'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { LobbyComponent } from './routes/lobby/lobby.component';
+import { LoginComponent } from './routes/login/login.component';
+import {ZardProgressBarComponent} from "@shared/components/progress-bar/progress-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LobbyComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZardProgressBarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
