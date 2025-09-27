cddc18975976ef2718d32bacf23b5fa74f331d62
NUREONNA <thainara864@gmail.com>
2025-09-27 18:55:24 -0300

    migra páginas estáticas (login, register, lobby) para componentes Angular; adiciona assets e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LobbyComponent} from "./routes/lobby/lobby.component";
-import {LoginComponent} from "./routes/login/login.component";
+import {LobbyComponent} from './routes/lobby/lobby.component';
+import {LoginComponent} from './routes/login/login.component';
+import {RegisterComponent} from './routes/register/register.component';
 
 const routes: Routes = [
-  {path: '', component: LobbyComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '**', redirectTo: 'login'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'lobby', component: LobbyComponent },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/routes/lobby/lobby.component.ts
+++ src/app/routes/lobby/lobby.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './lobby.component.scss'
 })
 export class LobbyComponent {
+  avatar = 'assets/quest/img/Artes M.png';
+  avatarOptions = [
+    'assets/quest/img/Artes F.png',
+    'assets/quest/img/Artes M.png',
+    'assets/quest/img/Ciência F.png',
+    'assets/quest/img/Ciência M.png'
+  ];
+  playerName = 'Jogador1';
+  moedas = 100;
+  energia = 50;
+  dropdownOpen = false;
+  players = [
+    { avatar: 'assets/quest/img/Artes M.png', nome: 'Jogador 1', status: 'Conectado' },
+    { avatar: 'assets/quest/img/Mundo M.png', nome: 'Jogador 2', status: 'Aguardando...' }
+  ];
 
+  toggleDropdown(){ this.dropdownOpen = !this.dropdownOpen; }
+  changeProfilePic(src: string){
+    this.avatar = src; this.players[0].avatar = src; this.dropdownOpen = false;
+  }
+  mudarNome(){
+    const novo = prompt('Digite o novo nome do jogador:', this.playerName);
+    if(novo) { this.playerName = novo; this.players[0].nome = novo; }
+  }
+  criarSala(){ alert('Sala criada! (simulação)'); }
+  entrarSala(){ alert('Você entrou na sala! (simulação)'); }
+  abrirMissoes(){ alert('Abrindo Missões... (simulação)'); }
+  abrirTutorial(){ alert('Abrindo Tutorial... (simulação)'); }
+  abrirLoja(){ alert('Loja ainda não implementada'); }
+  abrirPasse(){ alert('Passe de Batalha ainda não implementado'); }
 }
